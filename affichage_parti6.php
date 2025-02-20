39072c9750fb054a98cdad7cfe3c12989776d766
Albatorius62 <scania2645@hotmail.fr>
2025-02-20 14:12:00 +0100

    exercice parti 6

---
+++ affichage_parti6.php
@@ -0,0 +1,20 @@
+
+<?php
+require_once 'Vehicule.php';
+require_once 'Quatre_roues.php';
+require_once 'Camion.php';
+require_once 'Action.php';
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$camion = new Camion("bleu", 10000, 2);
+
+$camion->ajouter_remorque(10);
+
+$camion->mettre_essence(100);
+
+$camion->repeindre("vert");
+
+Vehicule::afficher_attribut($camion);
+?>
